b647cf746b218ba6593d5206a0343d5add67c1df
Eduard Khiaev <eddietheone1984@gmail.com>
2019-05-20 17:56:00 +0300

    add permissions

--- api_example/languages/views.py
+++ api_example/languages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Language
 from .serializers import LanguageSerializer
 
@@ -9,3 +9,4 @@ from .serializers import LanguageSerializer
 class LanguageView(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
